7df71af49209096ee14e90e482daa978dc5ba42c
InvinciblePyro <samrexspivey@gmail.com>
2025-10-09 16:22:57 -0700

    button works and i used better template from prev assignmnet (Step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
+let rocks: number = 0;
 
+// Create basic HTML structure
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-  <button> 🧱
+  <h1>Slamming Your Head Against a Wall</h1>
+  <p>Rocks: <span id="counter">0</span></p>
+  <button id="increment">🧱</button>
 `;
+
+// Add click handler
+const button = document.getElementById("increment")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  // This looks like to a good place to add some logic!
+  rocks += 1;
+  counterElement.innerHTML = rocks.toString();
+  console.log("I have these thingies:", button, counterElement, rocks);
+});
